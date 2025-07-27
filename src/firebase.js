6e4d846e3dc150ae660ecbaaf21074903e4d51c6
S-15-77 <santhoshdayakar2002@gmail.com>
2025-07-27 00:08:25 -0400

    added onboarding

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAEwjHvet2qrqZG7npfDXzBLhR_9oB8yUo",
-  authDomain: "expense-tracker-3def3.firebaseapp.com",
-  projectId: "expense-tracker-3def3",
-  storageBucket: "expense-tracker-3def3.firebasestorage.app",
-  messagingSenderId: "832389353112",
-  appId: "1:832389353112:web:1c4e189a4e974ecc9e8aed",
-  measurementId: "G-1E1CMV1DSH"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
